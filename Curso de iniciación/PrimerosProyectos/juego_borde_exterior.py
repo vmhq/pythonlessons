57fbf9d8d6e5593b9f29afe5c006c2732f98e47d
Vicente MÃendez <vicente@vmhq.cl>
2023-10-18 00:39:16 -0300

    Juego terminado, se actualiza código con las correciones.

--- Curso de iniciación/PrimerosProyectos/juego_borde_exterior.py
+++ Curso de iniciación/PrimerosProyectos/juego_borde_exterior.py
@@ -1,5 +1,6 @@
-titulo = "Escape del borde exterior galáctico"
+import random
 
+titulo = "Escape del borde exterior galáctico"
 print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
 
 #Se presenta al jugador la introducción a la historia.
@@ -19,28 +20,51 @@ opcion = input("¿Qué escoges? [A/B] \n").upper()
 #Se desprenden las condiciones de acuerdo al camino que haya escogido el jugador.
 if opcion == "A":
       print("Te adrentas en el bosque, en donde observas una edificación antigua, a la cual ingresas. \n" 
-            "Al interior encuentras una especie de dispositivo de comunicaciones \n" 
+            "Al interior encuentras una especie de dispositivo de comunicaciones. \n" 
             "Tienes dos opciones: \n" 
             "A: Activas el dispositivo \n" 
             "B: No activas el dispositivo \n")
-      opcion_caminob = input("¿Qué escoges? [A/B]").upper()
-      if opcion_caminob == "A":
+      opcion_caminoa = input("¿Qué escoges? [A/B] \n").upper()
+      if opcion_caminoa == "A":
             print("Al activar el dispositivo, este comienza a emitir una luz brillante. \n"
-                  "Está luz se intensifica de tal forma que emite un detello... \n"
+                  "Está luz se intensifica de tal forma que emite un destello... \n"
                   "¡Estás muerto! \n")
-      elif opcion_caminob == "B":
-            print("Al no activar el dispositivo de comunicaciones, no logras tomar contacto \n"
-                  "Te quedas en planeta, al no tener suminsitros finalmente al paso del tiempo mueres de hambre \n")
+      elif opcion_caminoa == "B":
+            print("Al no activar el dispositivo de comunicaciones, no logras tomar contacto con su receptor. \n"
+                  "Te quedas en planeta, al no tener suminsitros finalmente al paso del tiempo y mueres de hambre. \n")
 
 #Se desprenden las condiciones para el segundo camino posible.
 elif opcion == "B":
-      print("Al exporar los alrededores de la nave, encuentra un artefacto, el cual tiene el aspecto de un \n"
+      print("Al expLorar los alrededores de la nave, encuentras un artefacto el cual tiene el aspecto de un \n"
             "Traductor universal, pero al no conocer su origen dudas si tomarlo o no: \n"
             "A: Tomas el artefacto \n"
             "B: No tomas el artefacto \n")
-      opcion_inventario = input("¿Lo tomas? [A/B]").upper()
+      artefacto_inventario = input("¿Lo tomas? [A/B] \n").upper()
 
-      artefacto_inventario = False
+      panel_numero_random = random.randint (1, 10)
+      print("Al seguir buscando en los alrededores, encuentras una estructura la cual cuenta con un panel \n"
+            "En el panel alienigina te muestra un problema matemático para desbloquearlo \n"
+            "Cuanto es 5 * {}" .format(panel_numero_random))
+      resultado_puzzle = int(input("¿Cuál es el resultado? \n"))
 
-      if opcion_inventario == "A":
-            print("")
+      if resultado_puzzle == 5 * panel_numero_random and artefacto_inventario == "A":
+            print("El panel se abre y despliega un mapa. \n"
+                  "Como cuentas con el artefacto de traducción, logras decifrar el mapa, \n" ""
+                  "lo que te permite ubicar una estación de comunicación cercana y asi salir del planeta \n")
+      elif resultado_puzzle == 5 * panel_numero_random and artefacto_inventario == "B":
+            print("El panel se abre y despliega un mapa, pero al no tomar el artefacto de traducción, \n"
+                  "no cuentas con una herramienta para decifrarlo, por lo que te quedas sin suministros. \n"
+                  "Al final del paso dle tiempo mueres. \n")
+      elif resultado_puzzle != 5 * panel_numero_random and artefacto_inventario == "A":
+            print("Al no lograr desbloquear el panel, no obtienes lo que este almacena, por lo que decides forzarlo. \n"
+                  "Al forzarlo el panel emite una descarga eléctrica, la cual te mata al instante. \n")
+      elif resultado_puzzle != 5 * panel_numero_random and artefacto_inventario == "B":
+            print("Al no contar con el artefacto de traducción y no lograr desbloquer el panel, \n"
+                  "Implica que te quedes sin suministros, por lo que al paso del tiempo mueres de inanición. \n")
+      else:
+            print("Mueres repentinamente...")
+            exit()
+
+else:
+      print("No has escogido una respuesta válida, por lo que no queda mas que morir.")
+      exit()
