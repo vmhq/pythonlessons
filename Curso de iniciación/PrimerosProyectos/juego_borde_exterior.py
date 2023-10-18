52a5bf64896196f497f7b8b616c0909b73abf67a
Vicente MÃendez <vicente@vmhq.cl>
2023-10-17 22:41:01 -0300

    Se comienza a escribir códigos de juego borde exterior galáctico

---
+++ Curso de iniciación/PrimerosProyectos/juego_borde_exterior.py
@@ -0,0 +1,17 @@
+titulo = "Escape del borde exterior galáctico"
+
+print("\n" + titulo + "\n" + "-" * len(titulo) + "\n")
+
+#Se presenta al jugador la introducción a la historia.
+print("Al levantar la vista luego del impacto,"
+      "te das cuenta que te encuentras en la superficie de un planeta desconocido, tomando conocimiento de que \n"
+      "tu nave espacial se estrellado." + "\n")
+print("Observas a tu alrededor, a lo lejos observas un bosque frondoso, el cual podrías explorar \n" 
+      "o podrías explorar los alrededor de la nave en busca de suministros para reparar las comunicaciones \n")
+
+#Se presente al jugador la primera desición que debe tomar.
+print("Tienes dos caminos \n" 
+      "A: Te adentras en bosque para explorarlo \n" 
+      "B: Explorar los alrededores en busca de suministros \n")
+
+opcion = input("¿Qué escoges? [A/B] \n").upper()
