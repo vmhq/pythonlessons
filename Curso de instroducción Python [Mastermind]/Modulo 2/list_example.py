521036611ca84a2ce370adf5aec6837ef894b90a
Vicente Mendez <vicente@vmhq.cl>
2023-10-28 03:15:13 -0300

    explicación del código

--- Curso de instroducción Python [Mastermind]/Modulo 2/list_example.py
+++ Curso de instroducción Python [Mastermind]/Modulo 2/list_example.py
@@ -1,4 +1,22 @@
+'''
+Este es un código Python básico que utiliza una estructura de bucle for para recorrer una lista de números 
+y luego imprimir cada número.
+
+Desglose:
+
+numeros = [1, 2, 3]: Aquí estamos definiendo una lista llamada numeros que contiene tres elementos: 1, 2 y 3.
+
+for numero in numeros:: Aquí comenzamos un bucle for. La sintaxis for numero in numeros significa: 
+"Para cada numero en la lista numeros..."
+
+print(numero): Esto es lo que queremos hacer con cada numero en la lista numeros. 
+En este caso, simplemente estamos imprimiendo el número.
+
+'''
+
+#Se establece la lista
 numeros = [1, 2, 3]
 
+#Bucle for
 for numero in numeros:
     print(numero)
