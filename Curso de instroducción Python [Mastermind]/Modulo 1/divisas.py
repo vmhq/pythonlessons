574aae2d5e7970c4bfa6d8bda503dc61929938a3
Vicente Mendez <vicente@vmhq.cl>
2023-10-27 23:39:11 -0300

    se agregan nuevas divisas

--- Curso de instroducción Python [Mastermind]/Modulo 1/divisas.py
+++ Curso de instroducción Python [Mastermind]/Modulo 1/divisas.py
@@ -1,19 +1,31 @@
-dolar_euro = 0.95
+dolar_euro = 0.85
 libra_euro = 1.15
+dolar_clp = 925
+euro_clp = 900
 
-print("¿Qué deseas convertir:\n" 
-      "A: Dolar a euro \n" 
-      "B: Libra a euro \n")
+print("¿Qué deseas convertir:\n"
+      "A: Dólar a euro\n"
+      "B: Libra a euro\n"
+      "C: Dólar a peso chileno\n"
+      "D: Euro a peso chileno\n")
 
-respuesta = (input("Ingresa tu respuesta: \n"))
+respuesta = input("Ingresa tu respuesta: ").upper()
 
 if respuesta == "A":
-    dolares = (float(input("Ingresa la cantidad de dolares: \n")))
-    conversion = dolar_euro * dolares
-    print(("¨{} euros son {} dolares" .format(dolares, conversion)))
+    dolares = float(input("Ingresa la cantidad de dólares: "))
+    conversion = dolares * dolar_euro
+    print("{} dólares son {:.2f} euros".format(dolares, conversion))
 elif respuesta == "B":
-    libras = (float(input("Ingresa la cantidad de libras: \n")))
-    conversion = libra_euro * libras
-    print("{} libras son {} euros" .format(libras, conversion))
+    libras = float(input("Ingresa la cantidad de libras: "))
+    conversion = libras * libra_euro
+    print("{} libras son {:.2f} euros".format(libras, conversion))
+elif respuesta == "C":
+    dolares = float(input("Ingresa la cantidad de dólares: "))
+    conversion = dolares * dolar_clp
+    print("{} dólares son {:.2f} pesos chilenos".format(dolares, conversion))
+elif respuesta == "D":
+    euros = float(input("Ingresa la cantidad de euros: "))
+    conversion = euros * euro_clp
+    print("{} euros son {:.2f} pesos chilenos".format(euros, conversion))
 else:
-    print("No ha escogido ninguna opción valida.")
+    print("No ha escogido ninguna opción válida.")
