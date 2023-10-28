05d5e9e6a7586d60d6e6934cf75cc8271708bf45
Vicente Mendez <vicente@vmhq.cl>
2023-10-27 23:49:44 -0300

    se agrega ars to clp

--- Curso de instroducción Python [Mastermind]/Modulo 1/divisas.py
+++ Curso de instroducción Python [Mastermind]/Modulo 1/divisas.py
@@ -2,12 +2,14 @@ dolar_euro = 0.85
 libra_euro = 1.15
 dolar_clp = 925
 euro_clp = 900
+ars_clp = 2.65
 
 print("¿Qué deseas convertir:\n"
       "A: Dólar a euro\n"
       "B: Libra a euro\n"
       "C: Dólar a peso chileno\n"
-      "D: Euro a peso chileno\n")
+      "D: Euro a peso chileno\n"
+      "E: Peso argentino a peso chileno\n")
 
 respuesta = input("Ingresa tu respuesta: ").upper()
 
@@ -27,5 +29,9 @@ elif respuesta == "D":
     euros = float(input("Ingresa la cantidad de euros: "))
     conversion = euros * euro_clp
     print("{} euros son {:.2f} pesos chilenos".format(euros, conversion))
+elif respuesta == "E":
+    pesos_argentinos = float(input("Ingresa la cantidad de pesos argentinos: "))
+    conversion = pesos_argentinos * ars_clp
+    print("{} pesos argentinos son {:.2f} pesos chilenos".format(pesos_argentinos, conversion))
 else:
     print("No ha escogido ninguna opción válida.")
