8692c9d12595c57e017011adc66e5aa15f28e4f5
Vicente Méndez <vicente@vmhq.cl>
2023-11-06 22:53:35 -0300

    update code

---
+++ Curso de introducción Python [Mastermind]/Módulo 3/diccionario_ejemplo.py
@@ -0,0 +1,9 @@
+mi_diccionario = {
+    'nombre': 'Vicente',
+    'edad': 27,
+    'profesion': 'Psicólogo'
+}
+
+print(mi_diccionario['nombre'])  
+print(mi_diccionario['edad'])  
+print(mi_diccionario['profesion'])  
