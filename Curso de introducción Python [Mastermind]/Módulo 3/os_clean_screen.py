be24dfe47a7761eb47aca9834cdb2e022ebbd723
Vicente Méndez <vicente@vmhq.cl>
2023-11-06 23:18:36 -0300

    update code

---
+++ Curso de introducción Python [Mastermind]/Módulo 3/os_clean_screen.py
@@ -0,0 +1,10 @@
+import os
+
+
+def clean_screen():
+    os.system('clear') if os.name == 'posix' else os.system('cls') #Limpia la pantalla 
+
+
+if __name__ == "__main__":
+    def clean_screen()
+    
